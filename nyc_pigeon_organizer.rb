92ddb64ebd6857c202768d83bb09eeb910d27307
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-01 18:58:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,18 +3,76 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |feature, array|
       array.each do |name|
-
-        if pigeon_list.has_key?(name)
-          if pigeon_list[name].has_key?(key)
-            pigeon_list[name][key] << feature.to_s
-          else
-            pigeon_list[name][key] = [feature.to_s]
-          end
-        else
-          pigeon_list[name] = {key => [feature.to_s]}
+        if pigeon_list[name] == nil 
+          pigeon_list[name] = {}
+        end 
+        if pigeon_list[name][key] == nil
+          pigeon_list[name][key] = []
         end
+        pigeon_list[name][key] << feature.to_s
       end
     end
   end
   pigeon_list
 end
+
+
+
+pigeon_list = {
+  "Theo" => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway"]
+  },
+  "Peter Jr." => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Library"]
+  },
+  "Lucky" => {
+    :color => ["purple"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  },
+  "Ms. K" => {
+    :color => ["grey", "white"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
+  },
+  "Queenie" => {
+    :color => ["white", "brown"],
+    :gender => ["female"],
+    :lives => ["Subway"]
+  },
+  "Andrew" => {
+    :color => ["white"],
+    :gender => ["male"],
+    :lives => ["City Hall"]
+  },
+  "Alex" => {
+    :color => ["white", "brown"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  }
+}
+
+
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
