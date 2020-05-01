e79a924574683de7e530bef5377d73566a081088
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-01 19:01:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,16 +3,13 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |feature, array|
       array.each do |name|
-
-        if pigeon_list.has_key?(name)
-          if pigeon_list[name].has_key?(key)
-            pigeon_list[name][key] << feature.to_s
-          else
-            pigeon_list[name][key] = [feature.to_s]
-          end
-        else
-          pigeon_list[name] = {key => [feature.to_s]}
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
         end
+        if pigeon_list[name][key] == nil
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key] << feature.to_s
       end
     end
   end
